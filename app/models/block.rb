552c696b87e5df5961c32836150d018eeff2318f
Evgeniy Leontiev <potehin@gmail.com>
2018-01-09 17:21:08 +0400

    orphane blocks processing

--- app/models/block.rb
+++ app/models/block.rb
@@ -5,6 +5,12 @@ class Block < ApplicationRecord
     Bitcoin::P::Block.from_hash(self.bldata)
   end
 
+  def orphane?
+    orphane=false
+    self.bldata["tx"].map { |tx| Bitcoin::P::Tx.from_hash(tx,false).hash != tx["hash"]? orphane=true : nil }
+    orphane
+  end
+
   def total_value
     sum = 0
     self.showbl.tx.map { |tx| tx.outputs.map { |e| sum+=e.value } }
---
+++ test_orphan.rb
@@ -0,0 +1,42 @@
+
+
+orhp = {
+  "tx" => [
+    {
+      "in" => [
+        {
+          "coinbase" => "03963113055a53e183010000000000000000",
+          "prev_out" => {
+            "n"=>4294967295,
+            "hash"=>"0000000000000000000000000000000000000000000000000000000000000000"
+            }
+        }],
+      "out" => [
+        {
+          "value" => "1.56250000",
+          "scriptPubKey" => "OP_DUP OP_HASH160 ec2f9ffaba0d68ea6bd7c25cedfe2ae710938e60 OP_EQUALVERIFY OP_CHECKSIG"
+        },
+        {
+          "value"=>"0.00000000",
+          "scriptPubKey"=>"2 3 238:41:52534b424c4f434b3a9767eb468657db369e972885a7b43cabbdf48606b7906ede2815e25cd56f0ec8"
+        }
+        ],
+        "ver"=>1,
+        "hash"=>"17df42b8ce28f627f2443586ed0b0896102ec6a4fee2fc58bb6e402f6fe4bc1d",
+        "size"=>153,
+        "vin_sz"=>1,
+        "vout_sz"=>2,
+        "lock_time"=>0
+      }
+      ],
+    "ver"=>4,
+    "bits"=>486604799,
+    "hash"=>"000000001ccac7ba51816990cd87c8e1818552ddb49a847faaf067e46704055f",
+    "n_tx"=>1,
+    "size"=>234,
+    "time"=>1515446659,
+    "nonce"=>2329199811,
+    "mrkl_root"=>"17df42b8ce28f627f2443586ed0b0896102ec6a4fee2fc58bb6e402f6fe4bc1d",
+    "mrkl_tree"=>["17df42b8ce28f627f2443586ed0b0896102ec6a4fee2fc58bb6e402f6fe4bc1d"],
+    "prev_block"=>"000000006d77ed420651c95fea817adc48bb5783cbc25e6846c8f4db8022f210"
+  }
